6bef5a515b964c86730d8b70ae320b1b4a5fcaaa
Sera91 <serafina.digioia@inaf.it>
2024-05-28 17:18:29 +0200

    modified:   Day0/setup_conda.sh

--- Day0/setup_conda.sh
+++ Day0/setup_conda.sh
@@ -14,7 +14,8 @@ module load nccl/2.14.3-1--gcc--11.3.0-cuda-11.8
 module load gsl/2.7.1--gcc--11.3.0-omp
 
 #Step 1: Load my environment from the public area 
-conda activate /leonardo/pub/userexternal/sdigioia/sdigioia/env/Gabenv
+
+conda activate /leonardo/pub/usertrain/a08trc01/env/SMR3941
 
 
 #Step 2: Setting Jupyter server configuration for your account
